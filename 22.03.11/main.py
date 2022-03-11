00c3d55860626d2f3ed6484b9fe05789eb294d7d
Heetae Kim <aproda91@gmail.com>
2022-03-11 16:12:55 +0900

    220311 update

--- 22.03.11/main.py
+++ 22.03.11/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+
+from flask import Flask, redirect, render_template, request, redirect, url_for
+import pymysql
 
 app = Flask(__name__)
 
@@ -8,8 +10,38 @@ def index():
     return render_template("index.html")
 
 #localhost/signup로 접속했을때
-@app.route("/signup/")
+@app.route("/signup/", methods=["GET"])
 def signup():
     return render_template("signup.html")
+
+@app.route("/signup", methods=["POST"])
+def signup_2():
+    
+    _db = pymysql.connect(
+    user = "root",
+    passwd = "1234",
+    host = "localhost",
+    db = "ubion"
+    )
+
+    cursor = _db.cursor(pymysql.cursors.DictCursor)
+
+    _id = request.form["_id"]
+    _password = request.form["_password"]
+    _name = request.form["_name"]
+    _phone = request.form["_phone"]
+    _gender = request.form["_gender"]
+    _age = request.form["_age"]
+    _ads = request.form["_ads"]
+    _regitdate = request.form["_regitdate"]
+    sql = """
+            INSERT INTO user_info VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    _values = [_id, _password, _name, _phone, _ads, _gender, _age, _regitdate]
+    cursor.execute(sql, _values)
+    _db.commit()
+    _db.close()
+
+    return redirect(url_for('index'))
 
 app.run(port="80")
